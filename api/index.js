1bfbf4980a3db48c09d281346e1152f3f1fecd62
saamdanii <youremail@example.com>
2026-02-03 19:21:29 +0530

    CTF initial commit

---
+++ api/index.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  if (req.method === "HEAD") {
+    res.setHeader("X-Marauders-Map", "EXC{th3_r34l_fl@g!}");
+    return res.status(200).end();
+  }
+
+  return res.status(404).json({ message: "Nothing here" });
+}
