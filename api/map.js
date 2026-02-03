dcfa3a6c8d059aaa3801e8113a7ca366cc17a272
saamdanii <youremail@example.com>
2026-02-03 20:38:25 +0530

    Fix: include vercel.json and updated map logic

--- api/map.js
+++ api/map.js
@@ -1,8 +1,11 @@
 export default function handler(req, res) {
   if (req.method === "HEAD") {
+    // Setting the custom header for the flag
     res.setHeader("X-Marauders-Map", "EXC{th3_r34l_fl@g!}");
+    // HEAD requests MUST NOT return a body, so we use .end()
     return res.status(200).end();
   }
 
+  // Fallback for GET or other methods
   return res.status(404).send("Nothing here");
 }
